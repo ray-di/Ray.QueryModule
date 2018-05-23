7fb852edac0b15e4f8e67b8906674ded43febae7
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-05-23 14:52:27 +0900

    add WebQueryModule

---
+++ src/WebQuery.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of the Ray.Query.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace Ray\Query;
+
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
+use Ray\Query\Exception\WebQueryException;
+
+final class WebQuery implements QueryInterface
+{
+    /**
+     * @var ClientInterface
+     */
+    private $client;
+
+    /**
+     * @var string
+     */
+    private $method;
+
+    /**
+     * @var string
+     */
+    private $uri;
+
+    public function __construct(ClientInterface $client, string $method, string $uri)
+    {
+        $this->client = $client;
+        $this->method = $method;
+        $this->uri = $uri;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __invoke(array $query) : iterable
+    {
+        try {
+            $response = $this->client->request($this->method, $this->uri, ['query' => $query]);
+            $body = $response->getBody()->getContents();
+
+            return json_decode($body, true);
+        } catch (GuzzleException $e) {
+            throw new WebQueryException($e->getMessage(), 0, $e);
+        }
+    }
+}
---
+++ tests/WebQueryModuleTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of the Ray.Query.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace Ray\Query;
+
+use MongoDB\Driver\Query;
+use PHPUnit\Framework\TestCase;
+use Ray\Di\AbstractModule;
+use Ray\Di\Injector;
+
+class WebQueryModuleTest extends TestCase
+{
+    /**
+     * @var AbstractModule
+     */
+    private $module;
+
+    public function setUp()
+    {
+        $webQueryConfig = [
+            'foo' => ['GET', 'https://httpbin.org/anything/foo'],
+            'bar' => ['GET', 'https://httpbin.org/anything/bar']
+        ];
+        $guzzleConfig = [];
+        $this->module = new WebQueryModule($webQueryConfig, $guzzleConfig);
+    }
+
+    public function testQueryInterface()
+    {
+        $foo = (new Injector($this->module))->getInstance(QueryInterface::class, 'foo');
+        $this->assertInstanceOf(QueryInterface::class, $foo);
+        $result = $foo([]);
+        $this->assertSame('https://httpbin.org/anything/foo', $result['url']);
+    }
+
+    public function testCallable()
+    {
+        $foo = (new Injector($this->module))->getInstance('', 'foo');
+        $this->assertInternalType('callable', $foo);
+        $result = $foo([]);
+        $this->assertSame('https://httpbin.org/anything/foo', $result['url']);
+    }
+}
