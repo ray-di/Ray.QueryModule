8e1b379d9066769a594b8e3db7a3e34a2e244109
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-06-01 11:27:03 +0900

    add example code

---
+++ tests/Fake/ExampleQuery.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of the Ray.Query.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace Ray\Query;
+
+use Ray\Query\Annotation\Query;
+
+class ExampleQuery
+{
+    public function listNames(int $age) : void
+    {
+        $users = $this->getUsers($age);
+        foreach ($users as $user) {
+            echo $user['name'] . PHP_EOL;
+        }
+    }
+
+    /**
+     * @Query(id="users_by_age", type="row_list")
+     *
+     * @return array<array{id: string, name: string}>
+     * @psalm-suppress InvalidReturnType
+     */
+    public function getUsers(int $age) : array
+    {
+        unset($age);
+    }
+}
