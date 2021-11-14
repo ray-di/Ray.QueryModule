dbf1f3d11a120a6eba4baef352a86bb61891612a
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-11-14 22:50:11 +0900

    Suppress E_DEPRECATED in vendor files
    Aura.Sql currently not supported PHP 8.1

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use Koriym\Attributes\AttributeReader;
 use Ray\ServiceLocator\ServiceLocator;
 
+require dirname(__DIR__) . '/vendor/autoload.php';
+
 $rm = static function ($dir) use (&$rm) {
     foreach ((array) glob($dir . '/*') as $file) {
         $f = (string) $file;
@@ -12,7 +14,15 @@ $rm = static function ($dir) use (&$rm) {
         @rmdir($f);
     }
 };
+
 $rm(__DIR__ . '/tmp');
+
+// Suppress E_DEPRECATED in vendor files
+if (PHP_VERSION_ID >= 80100) {
+    set_error_handler(static function (int $errno, string $errstr, string $errfile) {
+        return $errno === E_DEPRECATED && str_contains($errfile, dirname(__DIR__) . '/vendor');
+    });
+}
 
 // no annotation in PHP 8
 if (PHP_MAJOR_VERSION >= 8) {
