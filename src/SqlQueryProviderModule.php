e2690073616b7edf79981257b8c74b2db3c9857b
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-03-03 08:54:38 +0900

    Remove koriym/param-reader dependency
    Replace ParamReaderInterface with native ReflectionParameter::getAttributes()
    since doctrine/annotations has been removed and only PHP 8 attributes are supported.

--- src/SqlQueryProviderModule.php
+++ src/SqlQueryProviderModule.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Ray\Query;
 
-use Koriym\ParamReader\ParamReader;
-use Koriym\ParamReader\ParamReaderInterface;
 use Ray\Di\AbstractModule;
 use Ray\Di\Scope;
 
@@ -23,7 +21,6 @@ final class SqlQueryProviderModule extends AbstractModule
     protected function configure()
     {
         $this->bind(SqlFinder::class)->in(Scope::SINGLETON);
-        $this->bind(ParamReaderInterface::class)->to(ParamReader::class)->in(Scope::SINGLETON);
         $this->bind(RowInterface::class)->toProvider(RowInterfaceProvider::class)->in(Scope::SINGLETON);
         $this->bind(RowListInterface::class)->toProvider(RowListInterfaceProvider::class)->in(Scope::SINGLETON);
         $this->bind(InvokeInterface::class)->toProvider(RowListInterfaceProvider::class)->in(Scope::SINGLETON);
